e79417d28d47b48075f33dfac774c12351831756
Alexander Maslov <steam3d@gmail.com>
2024-02-03 15:22:48 +0300

    Added BtVendorIds and AppleProductIds

---
+++ src/appleproductids.h
@@ -0,0 +1,25 @@
+#ifndef APPLEPRODUCTIDS_H
+#define APPLEPRODUCTIDS_H
+
+// Modalias: bluetooth:v{VendorIds}p{AppleProductIds}dB087
+enum class AppleProductIds: unsigned short{
+    airpods1 = 0x2002,
+    airpods2 = 0x200f, 
+    airpods3 = 0x2013, 
+    airpodspro = 0x200e,
+    airpodspro2 = 0x2014,
+    airpodsprousbc = 0x2024,
+    powerbeatspro = 0x200b,
+    airpodsmax = 0x200a,
+    beatsSolo3 = 0x2006,
+    beatssolopro = 0x200c,
+    beatsstudio3 = 0x2009,
+    beatsx = 0x2005,
+    beatsflex = 0x2010,
+    powerbeats3 = 0x2003,
+    beatsfitpro = 0x2012,
+    beatsstudiobuds = 0x2011,
+    beatsstudiobudsplus = 0x2016,
+    powerbeats4 = 0x200d,
+};
+#endif
---
+++ src/btvendorids.h
@@ -0,0 +1,9 @@
+#ifndef BTVENDORIDS_H
+#define BTVENDORIDS_H
+
+// Modalias: bluetooth:v{BtVendorIds}p{AppleProductIds}dB087
+enum class BtVendorIds: unsigned short{
+ Apple = 0x004c,
+ Sony = 0x054c
+};
+#endif
