6150ff6762dc133498881906e4721eed8af9a89a
Andrey Litvintsev <10694658+andreylitvintsev@users.noreply.github.com>
2024-02-11 16:15:59 +0300

    Revert "Add communication with Aiprods using l2cap" (#3)
    This reverts commit b9d7308f4189f8ecdbe671d7771114fb4a59641d.

--- src/btvendorids.h
+++ src/btvendorids.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // Modalias: bluetooth:v{BtVendorIds}p{AppleProductIds}dB087
-enum class BtVendorIds : unsigned short {
+enum class BtVendorIds: unsigned short {
     Apple = 0x004c,
     Sony = 0x054c
 };
