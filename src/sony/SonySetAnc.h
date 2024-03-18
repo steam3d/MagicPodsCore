b9309a2f63fa32e31ce7bfb85830e590c54eb729
Alexander Maslov <steam3d@gmail.com>
2024-03-18 16:03:41 +0300

    Added SonySetAnc

---
+++ src/sony/SonySetAnc.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include "SonyBaseCmd.h"
+#include "SonyGetAnc.h"
+
+namespace MagicPodsCore
+{   
+    class SonySetAnc : public SonyBaseCmd {
+    public:
+        SonySetAnc(SonyAncState state);
+        SonyAncState state{};
+        void ProcessResponse(const std::vector<unsigned char> &bytes) override;
+    protected:
+        std::vector<unsigned char>CreatePacketBody(unsigned char prefix) const override;
+    };
+}
