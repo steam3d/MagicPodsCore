e6539d16c5e3b8e175ce68acaa4f5c1f8f202055
Alexander Maslov <steam3d@gmail.com>
2024-04-22 11:18:09 +0300

    Create LICENSE

--- src/aap/AapClient.cpp
+++ src/aap/AapClient.cpp
@@ -1,3 +1,11 @@
+//
+// MagicPodsCore
+// https://github.com/steam3d/MagicPodsCore
+//
+// Copyright: 2020-2024 Aleksandr Maslov <https://magicpods.app> & Andrei Litvintsev <a.a.litvintsev@gmail.com>
+// License: AGPL-3.O 
+//
+
 #include "AapClient.h"
 
 #include "Logger.h"
