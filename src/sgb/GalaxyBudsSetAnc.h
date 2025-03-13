220b4351bade1a0039e06d439159445403dbab1f
Alexander Maslov <steam3d@gmail.com>
2025-03-13 11:58:47 +0300

    Added GalaxyBudsSetAnc

---
+++ src/sgb/GalaxyBudsSetAnc.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "GalaxyBudsMsgIds.h"
+#include "GalaxyBudsModelIds.h"
+#include "GalaxyBudsAnc.h"
+#include <vector>
+
+namespace MagicPodsCore{
+    class GalaxyBudsSetAnc
+    {
+        public:
+            const std::vector<unsigned char>Payload;
+            const GalaxyBudsMsgIds Id = GalaxyBudsMsgIds::NOISE_CONTROLS;
+            explicit GalaxyBudsSetAnc(GalaxyBudsAnc mode): Payload{static_cast<unsigned char>(mode)} {}
+    };    
+}
