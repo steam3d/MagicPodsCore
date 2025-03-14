1c0246a809b1ace992a154500a4095f75f22ac08
Andrei Litvintsev <a.a.litvintsev@gmail.com>
2025-03-14 20:47:16 +0700

    Fixed merge

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "client/Client.h"
+#include "Logger.h"
 
 class CheckRequest : public MagicPodsCore::IRequest {
 public:
@@ -8,23 +9,19 @@ public:
     }
 };
 
-class CheckResponseWatcher : public MagicPodsCore::IResponseWatcher {
-public:
-    void ProcessBytes(const std::vector<unsigned char>& bytes) override {
-        // Just stub
-    }
-};
-
 int main() {
     using namespace MagicPodsCore;
 
     std::cout << "Hello" << std::endl;
 
-    const auto client = MagicPodsCore::Client::Create("24:29:34:BD:CD:0F", ClientConnectionType::RFCOMM, std::make_shared<CheckResponseWatcher>());
+    const auto client = MagicPodsCore::Client::CreateRFCOMM("24:29:34:BD:CD:0F", "df21fe2c-2515-4fdb-8886-f12c4d67927c");
+    client->GetOnReceviedDataEvent().Subscribe([](size_t id, const std::vector<unsigned char>& data) {
+        LOG_DEBUG("Data received %d", data.size());
+    });
     client->Start();
     
-    // std::this_thread::sleep_for(std::chrono::seconds(1));
-    // client->SendRequest(CheckRequest{});
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    client->SendRequest(CheckRequest{});
 
     std::this_thread::sleep_for(std::chrono::seconds(100));
 
